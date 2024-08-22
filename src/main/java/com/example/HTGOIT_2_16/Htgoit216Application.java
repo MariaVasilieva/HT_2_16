11dc1d761cf92cb955630976b9be862ea3a47d09
mdvas <mdvasilieva@gmail.com>
2024-08-22 14:58:55 +0300

    Home task 2 module 16

---
+++ src/main/java/com/example/HTGOIT_2_16/Htgoit216Application.java
@@ -0,0 +1,13 @@
+package com.example.HTGOIT_2_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Htgoit216Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Htgoit216Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HTGOIT_2_16/Htgoit216ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HTGOIT_2_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Htgoit216ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
